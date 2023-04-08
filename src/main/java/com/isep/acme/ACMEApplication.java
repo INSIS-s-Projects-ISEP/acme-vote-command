b824879ba8015263ff764d1139b1191463f7f5b4
David Gomesh <davidgomesh2016@gmail.com>
2023-04-08 09:37:29 +0100

    Fixed service hosts

--- src/main/java/com/isep/acme/ACMEApplication.java
+++ src/main/java/com/isep/acme/ACMEApplication.java
@@ -2,8 +2,10 @@ package com.isep.acme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ACMEApplication {
 
 	public static void main(String[] args) {
